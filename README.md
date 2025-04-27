0235b9172b0665505b2ccb9f87faf10ae46382ef
Sandeep-060 <tharalasandeep2005@gmail.com>
2025-04-27 17:33:50 +0530

    intial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# SkyScope - Weather App
+
+SkyScope is a weather application that provides real-time weather information based on a city search or your current location. With an intuitive design and support for dark mode, SkyScope delivers a seamless experience for users to check weather conditions like temperature, humidity, wind speed, and weather description.
+
+## Features
+
+- **City Search**: Get weather details by searching for a city name.
+- **Current Location**: Automatically fetch weather data based on your current geographic location.
+- **Dark Mode**: Toggle between light and dark themes for a comfortable viewing experience.
+- **Weather Information**: Displays detailed weather information including temperature, weather description, humidity, and wind speed.
+- **Responsive Design**: Optimized for both mobile and desktop devices.
+
+## Tech Stack
+
+- **Frontend**: HTML, CSS, JavaScript
+- **Weather Data**: OpenWeatherMap API 
+
+
+## How It Works
+
+1. **Weather Data Fetching**: SkyScope pulls weather data from the OpenWeatherMap API  It can retrieve weather information based on the city entered in the search bar or the user’s current geographical location using the browser’s geolocation API.
+   
+2. **Dark Mode Toggle**: The app features a simple dark mode toggle to switch between light and dark themes. This can be easily activated by clicking the dark mode button in the app.
+
+3. **Responsive Design**: The app is built with responsiveness in mind, ensuring that it works seamlessly across different screen sizes, from mobile to desktop.
+
+
+
+
---
+++ script.js
@@ -0,0 +1,201 @@
+const searchBtn = document.querySelector(".searchbtn");
+const city = document.querySelector(".city-input");
+const container = document.querySelector(".container");
+const currentlocation = document.querySelector(".current-location-btn");
+const darkModeButton = document.querySelector(".darkmode")
+
+const apikey = 'a28e0099dfacbfc3659147c26775c76d';
+const weatherIconMap = {
+  Clear: "clear",
+  Clouds: "clouds",
+  Rain: "rain",
+  Thunderstorm: "thunderstorm",
+  Snow: "snow",
+  Mist: "fog",
+  Fog: "fog",
+  Haze: "fog",
+  Drizzle: "rain",
+};
+
+function getCityLocalTime(timezoneOffsetInSeconds) {
+  const now = new Date();
+  const utc = now.getTime() + (now.getTimezoneOffset() * 60000);
+  const cityTime = new Date(utc + timezoneOffsetInSeconds * 1000);
+
+  const options = {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: true
+  };
+  return cityTime.toLocaleString('en-GB', options);
+}
+
+function createWeatherDetails(data) {
+  const localTime = getCityLocalTime(data.timezone);
+  const weatherMain = data.weather[0].main;
+  const iconName = weatherIconMap[weatherMain] || "clear";
+  const imgSrc = `assets/${iconName}.png`;
+
+  const details = document.createElement("div");
+  details.classList.add("details");
+  details.innerHTML = `
+    <div class="city-d">
+      <h3><i class="fa-solid fa-location-dot"></i> ${data.name} <span id="country">${data.sys.country}</span></h3>
+      <p>${localTime}</p>
+    </div>
+    <div class="tem">
+      <img src="${imgSrc}" alt="">
+      <h1>${data.main.temp}<sup>o</sup></h1>
+      <p>${data.weather[0].description}</p>
+    </div>
+    <div class="other-d">
+      <div class="humidty">
+        <div class="pic"><i class="fa-solid fa-droplet"></i></div>
+        <div class="info">
+          <p>Humidity</p>
+          <p>${data.main.humidity}%</p>
+        </div>
+      </div>
+      <div class="wind">
+        <div class="pic"><i class="fa-solid fa-wind"></i></div>
+        <div class="info">
+          <p>Wind Speed</p>
+          <p>${data.wind.speed} m/s</p>
+        </div>
+      </div>
+    </div>
+  `;
+  return details;
+}
+
+function createForecast(forecastData) {
+  const totalForecast = document.createElement("div");
+  totalForecast.classList.add("total-forecast");
+
+  const heading = document.createElement("h3");
+  heading.classList.add("fore");
+  heading.textContent = "Forecast";
+
+  const forecast = document.createElement("div");
+  forecast.classList.add("forecast");
+
+  const dailyForecasts = forecastData.list.filter(item => item.dt_txt.includes("12:00:00"));
+
+  dailyForecasts.forEach(element => {
+    const date = new Date(element.dt_txt);
+    const dayName = date.toLocaleDateString("en-US", { weekday: "short" });
+    const weatherMainForecast = element.weather[0].main;
+    const iconNameForecast = weatherIconMap[weatherMainForecast] || "clear";
+    const imgSrcForecast = `assets/${iconNameForecast}.png`;
+
+    forecast.innerHTML += `
+      <div class="day">
+        <p>${dayName}</p>
+        <img src="${imgSrcForecast}" alt="">
+        <p>${element.weather[0].main}</p>
+        <p>${element.main.temp}<sup>o</sup></p>
+      </div>
+    `;
+  });
+
+  totalForecast.append(heading, forecast);
+  return totalForecast;
+}
+
+function clearOldData() {
+  const oldDetails = document.querySelector(".details");
+  if (oldDetails){
+    oldDetails.remove();
+  } 
+  const oldForecast = document.querySelector(".total-forecast");
+  if (oldForecast){
+    oldForecast.remove();
+  } 
+}
+
+async function fetchWeatherAndForecast(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      alert("City not found");
+      throw new Error("City not found");
+    }
+
+    const data = await response.json();
+    clearOldData();
+    container.append(createWeatherDetails(data));
+
+    let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=${apikey}&units=metric`;
+    
+    const forecastResponse = await fetch(forecastUrl);
+    const forecastData = await forecastResponse.json();
+    container.append(createForecast(forecastData));
+
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+  finally{
+    document.querySelector('.total-forecast').scrollIntoView({ behavior: 'smooth' })
+  }
+}
+
+async function getData() {
+  const cityValue = city.value.trim();
+  if (cityValue === "") {
+    alert("Please enter city");
+    city.focus();
+    return;
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`;
+  await fetchWeatherAndForecast(url);
+}
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`;
+      fetchWeatherAndForecast(url);
+    }, error => {
+      alert("Grant permission to enable location");
+      console.error("Geolocation Error:", error.message);
+    });
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+}
+
+function getdark(){
+  const body=document.body
+  body.classList.toggle("dark-mode")
+
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+ 
+}
+
+function savedtheme(){
+  if (localStorage.getItem("theme") === "dark") {
+   document.body.classList.add("dark-mode");
+  }
+}
+
+searchBtn.addEventListener("click", getData);
+city.addEventListener("keypress", event => {
+  if (event.key === "Enter"){
+    getData();
+  } 
+});
+currentlocation.addEventListener("click", getLocation);
+
+darkModeButton.addEventListener("click",getdark)
+
+window.addEventListener("load",savedtheme)
